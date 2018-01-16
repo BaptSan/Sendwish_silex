914c990f545ac2ffb4971951f3876b4c86a413e9
Unknown <pillot.clement@gmail.com>
2018-01-16 10:42:26 +0100

    Fixed the successfull message, got erased by a previous push

--- src/controllers.php
+++ src/controllers.php
@@ -7,8 +7,10 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Entity\User;
 
 //Request::setTrustedProxies(array('127.0.0.1'));
-$app->get('/', function () use ($app) {
-return $app['twig']->render('accueil.html.twig', array());
+$app->get('/', function () use ($app) {     
+    return $app['twig']->render('index.html.twig', array( 
+                        'isRegister' => $_GET['register'] ?? NULL  
+                    )); 
 })
 ->bind('homepage');
 
