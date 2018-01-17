4779cc4b3805854dceb1b6cb757341c904cfb8b5
Unknown <baptiste.sanchez72@gmail.com>
2018-01-17 16:48:31 +0100

    création de la page twig + début fonction ajout panier.

--- web/js/index.js
+++ web/js/index.js
@@ -6,6 +6,18 @@ initFlick();
  	$('.carousel-main').flickity({
     pageDots: true,
     });
+  $('#secondCar .card img').click(function() {  
+    //console.log('test');
+    idClique=$(this).attr('data-id');
+    console.log(idClique);
+    // if($(this).hasClass("img1")) {
+    //   console.log('test1');
+    // if($(this).hasClass("img2")) {
+    //   console.log('test2');
+    //  }
+    // }
+    
+  });
   
   // 2nd carousel, navigation
  $('.carousel-nav').flickity({
@@ -15,42 +27,40 @@ initFlick();
 }
  
 $( "#card1" ).click(function() {
-	console.log('test');
 $('#secondCar').html('<div class="carousel carousel-main">'+'<div class="carousel-cell card img1">'+
-          '<img src="../img/boisson/fanta.png" alt="" class="w-25 align-self-center" >'+
+          '<img data-id="1" src="../img/boisson/fanta.png" alt="" class="w-25 align-self-center" >'+
         '</div>'+
         '<div class="carousel-cell card img2">'+
-          '<img src="../img/boisson/coca.png" alt="" class="w-25 align-self-center">'+
+          '<img data-id="2" src="../img/boisson/coca.png" alt="" class="w-25 align-self-center">'+
         '</div>'+
         '<div class="carousel-cell card img3">'+
-          '<img src="../img/boisson/cocalight.png" alt="" class="w-25 align-self-center">'+
+          '<img data-id="3" src="../img/boisson/cocalight.png" alt="" class="w-25 align-self-center">'+
         '</div>'+
         '<div class="carousel-cell card img4">'+
-          '<img src="../img/boisson/nestea.png" alt="" class="w-25 align-self-center">'+
+          '<img data-id="4" src="../img/boisson/nestea.png" alt="" class="w-25 align-self-center">'+
         '</div>'+
         '<div class="carousel-cell card img5">'+
-          '<img src="../img/boisson/sprite.png" alt="" class="w-25 align-self-center">'+
+          '<img data-id="5" src="../img/boisson/sprite.png" alt="" class="w-25 align-self-center">'+
         '</div>'+'</div>');
 initFlick();
 
 });
 
 $( "#card2" ).click(function() {
-	console.log('test');
 $('#secondCar').html('<div class="carousel carousel-main">'+'<div class="carousel-cell card img1">'+
-          '<img src="../img/menu/bigking.png" alt="" class="w-25 align-self-center" >'+
+          '<img data-id="6" src="../img/menu/bigking.png" alt="" class="w-25 align-self-center" >'+
         '</div>'+
         '<div class="carousel-cell card img2">'+
-          '<img src="../img/menu/chickeneggburger.png" alt="" class="w-25 align-self-center">'+
+          '<img data-id="7" src="../img/menu/chickeneggburger.png" alt="" class="w-25 align-self-center">'+
         '</div>'+
         '<div class="carousel-cell card img3">'+
-          '<img src="../img/menu/doubleeggburger.png" alt="" class="w-25 align-self-center">'+
+          '<img data-id="8" src="../img/menu/doubleeggburger.png" alt="" class="w-25 align-self-center">'+
         '</div>'+
         '<div class="carousel-cell card img4">'+
-          '<img src="../img/menu/egg burger.png" alt="" class="w-25 align-self-center">'+
+          '<img data-id="9" src="../img/menu/egg burger.png" alt="" class="w-25 align-self-center">'+
         '</div>'+
         '<div class="carousel-cell card img5">'+
-          '<img src="../img/menu/whopper.png" alt="" class="w-25 align-self-center">'+
+          '<img data-id="10" src="../img/menu/whopper.png" alt="" class="w-25 align-self-center">'+
         '</div>'+'</div>');
 initFlick();
 
@@ -58,40 +68,37 @@ initFlick();
 
 
 $( "#card3" ).click(function() {
-	console.log('test');
 $('#secondCar').html('<div class="carousel carousel-main">'+
         '<div class="carousel-cell card img2">'+
-          '<img src="../img/accompagnement/chilicheese.png" alt="" class="w-25 align-self-center">'+
+          '<img data-id="11" src="../img/accompagnement/chilicheese.png" alt="" class="w-25 align-self-center">'+
         '</div>'+
         '<div class="carousel-cell card img3">'+
-          '<img src="../img/accompagnement/frites.png" alt="" class="w-25 align-self-center">'+
+          '<img data-id="12" src="../img/accompagnement/frites.png" alt="" class="w-25 align-self-center">'+
         '</div>'+
         '<div class="carousel-cell card img4">'+
-          '<img src="../img/accompagnement/nuggets.png" alt="" class="w-25 align-self-center">'+
+          '<img data-id="13" src="../img/accompagnement/nuggets.png" alt="" class="w-25 align-self-center">'+
         '</div>'+
         '<div class="carousel-cell card img5">'+
-          '<img src="../img/accompagnement/ognionring.png" alt="" class="w-25 align-self-center">'+
+          '<img data-id="14" src="../img/accompagnement/ognionring.png" alt="" class="w-25 align-self-center">'+
         '</div>'+'</div>');
 initFlick();
 
 });
 
 $( "#card4" ).click(function() {
-	console.log('test');
 $('#secondCar').html('<div class="carousel carousel-main">'+'<div class="carousel-cell card img1">'+
-          '<img src="../img/menu enfant/kingjunior.png" alt="" class="w-25 align-self-center" >'+
+          '<img data-id="15" src="../img/menu enfant/kingjunior.png" alt="" class="w-25 align-self-center" >'+
         '</div>');
 initFlick();
 
 });
 
 $( "#card5" ).click(function() {
-	console.log('test');
 $('#secondCar').html('<div class="carousel carousel-main">'+'<div class="carousel-cell card img1">'+
-          '<img src="../img/sandwich/cheeseburgerenfant.png" alt="" class="w-25 align-self-center" >'+
+          '<img data-id="16" src="../img/sandwich/cheeseburgerenfant.png" alt="" class="w-25 align-self-center" >'+
         '</div>'+
         '<div class="carousel-cell card img2">'+
-          '<img src="../img/sandwich/hamburgerenfant.png" alt="" class="w-25 align-self-center">'+
+          '<img data-id="17" src="../img/sandwich/hamburgerenfant.png" alt="" class="w-25 align-self-center">'+
         '</div>');
 initFlick();
 
