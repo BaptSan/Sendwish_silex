de0b106cf3be88cdcd3f65b25ff5f1d1babd2da2
Unknown <pillot.clement@gmail.com>
2018-01-18 09:56:43 +0100

    Using the constructor in the controllers to create a User when register

--- src/controllers.php
+++ src/controllers.php
@@ -52,7 +52,6 @@ $app->match('/connexion', function (Request $request) use ($app) {
 });
 
 $app->match('/inscription', function (Request $request) use ($app) {
-    $thevar = new User();
     if ($request->get('inscripValid') !== NULL) {
         if (null !== $request->get('firstname') && !empty($request->get('firstname')) &&
             null !== $request->get('inputLat') && !empty($request->get('inputLat')) &&
@@ -66,37 +65,23 @@ $app->match('/inscription', function (Request $request) use ($app) {
             null !== $request->get('password') && !empty($request->get('password')) &&
             null !== $request->get('passwordConf') && !empty($request->get('passwordConf')) &&
             $request->get('password') === $request->get('passwordConf')) {
-            $varFirstName = htmlspecialchars($request->get('firstname'));
-            $varLastName = htmlspecialchars($request->get('lastname'));
-            $varLat = htmlspecialchars($request->get('inputLat'));
-            $varLng = htmlspecialchars($request->get('inputLng'));
-            $varDist = htmlspecialchars($request->get('inputDist'));
-            $varAddress = htmlspecialchars($request->get('address'));
-            $varEmail = htmlspecialchars($request->get('email'));
-            $varTel = htmlspecialchars($request->get('tel'));
-            $varBirthday = htmlspecialchars($request->get('birthday'));
-            $varPassWord = htmlspecialchars($request->get('password'));
-            $varPassWordConf = htmlspecialchars($request->get('passwordConf'));
+                $varFirstName = htmlspecialchars($request->get('firstname'));
+                $varLastName = htmlspecialchars($request->get('lastname'));
+                $varLat = htmlspecialchars($request->get('inputLat'));
+                $varLng = htmlspecialchars($request->get('inputLng'));
+                $varDist = htmlspecialchars($request->get('inputDist'));
+                $varAddress = htmlspecialchars($request->get('address'));
+                $varEmail = htmlspecialchars($request->get('email'));
+                $varTel = htmlspecialchars($request->get('tel'));
+                $varBirthday = htmlspecialchars($request->get('birthday'));
+                $varPassWord = htmlspecialchars($request->get('password'));
+                $varPassWordConf = htmlspecialchars($request->get('passwordConf'));
+                $sd = new User($id, $varLastName, $varFirstName, $varEmail, password_hash($varPassWord, PASSWORD_DEFAULT), new DateTime($varBirthday), $varAddress, $varLat, $varLng, $varDist, $varTel, true, false, false, $cartItems, $orders);
+                $em = $app['em'];
+                $em->persist($sd);
+                $em->flush();
+            return $app->redirect('/?register=true');
         }
-        $sd = new User();
-        $em = $app['em'];
-        $sd->setFirstname($varFirstName);
-        $sd->setLastname($varLastName);
-        $sd->setMail($varEmail);
-        // $sd->setPassword($varPassWord);
-        $sd->setPassword(password_hash($varPassWord, PASSWORD_DEFAULT));
-        $sd->setTel($varTel);
-        $sd->setFormattedAddr($varAddress);
-        $sd->setBirthdate(new DateTime($varBirthday));
-        $sd->setLat($varLat);
-        $sd->setIsAdmin(true);
-        $sd->setIsClient(false);
-        $sd->setIsDelivery(false);
-        $sd->setLng($varLng);
-        $sd->setDistance($varDist);
-        $em->persist($sd);
-        $em->flush();
-        return $app->redirect('/?register=true');
     }
     return $app['twig']->render('inscription.html.twig', array());
 });
