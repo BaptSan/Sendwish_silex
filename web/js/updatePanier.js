6a2a1ac0311d9e6e28f512289ae0faf167d7589d
Unknown <pillot.clement@gmail.com>
2018-02-04 17:57:44 +0100

    Added all the code for Add/Remove Qty From Cart

--- web/js/updatePanier.js
+++ web/js/updatePanier.js
@@ -7,3 +7,56 @@ function updateCart(){
 			$('#pageSubCart').html(data);
 		});
 }
+
+$('.btQtyCartMinus').click(function(){
+	dataId = $(this).attr('data-id');
+	actualQty = $('.qtyInputCartId'+dataId).val();
+	if(actualQty == 1){
+		return false;
+	}
+	$.ajax({
+		type:'get',
+		url:'/removeFromCart',
+		data:{
+			'carrousel': dataId
+		}
+	}).done(function(data){
+		productId = data;
+		productQty = $('.qtyInputCartId'+productId).val() ;
+		totalProductPrice = $('.priceTotalProductId'+productId).attr('data-price');
+		unitPrice = totalProductPrice / productQty;
+		newTotalProductPrice = totalProductPrice-unitPrice;
+		$('.priceTotalProductId'+productId).attr('data-price',newTotalProductPrice.toFixed(1));
+		$('.priceTotalProductId'+productId).html(newTotalProductPrice.toFixed(1)+" €");
+		prixTotal-=unitPrice;
+		$('#totalPriceCart').html("Prix Total : "+prixTotal.toFixed(1)+"€");
+		updateCart();
+		$('.qtyInputCartId'+dataId).val(--actualQty);
+	});
+});
+
+$('.btQtyCartPlus').click(function(){
+	dataId = $(this).attr('data-id');
+	actualQty = $('.qtyInputCartId'+dataId).val();
+	$.ajax({
+		type:'get',
+		url:'/addToCart',
+		data:{
+			'carrousel': dataId
+		}
+	}).done(function(data){
+		productId = data;
+		productQty = parseInt($('.qtyInputCartId'+productId).val());
+		totalProductPrice = parseFloat($('.priceTotalProductId'+productId).attr('data-price'));
+		unitPrice = parseFloat((totalProductPrice/productQty).toPrecision(3));
+
+		newTotalProductPrice = totalProductPrice+unitPrice;
+		prixTotal = prixTotal+unitPrice;
+		$('.priceTotalProductId'+productId).attr('data-price',newTotalProductPrice.toFixed(1));
+		$('.priceTotalProductId'+productId).html(newTotalProductPrice.toFixed(1)+" €");
+		$('#totalPriceCart').html("Prix Total : "+prixTotal.toFixed(1)+"€");
+		console.log(prixTotal.toFixed(1));
+		updateCart();
+		$('.qtyInputCartId'+dataId).val(++actualQty);
+	});
+});
